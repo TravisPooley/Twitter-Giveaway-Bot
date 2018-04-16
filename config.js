36d35fc7207377afba6985e38be10c17d64f589e
Travis Pooley <Travispooley@gmail.com>
2018-04-16 19:39:36 +0930

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+{
+  "consumerKey":"", // Consumer Key From Twitter developer account (REQUIRED FOR BOT TO LOGIN TO YOUR ACCOUNT)
+  "consumerSecret":"", // Consumer Secret From Twitter developer account (REQUIRED FOR BOT TO LOGIN TO YOUR ACCOUNT)
+  "accessToken":"", // Aceess Token Key From Twitter developer account (REQUIRED FOR BOT TO LOGIN TO YOUR ACCOUNT)
+  "acessTokenSectret":"", // Access Token Secret Key From Twitter developer account (REQUIRED FOR BOT TO LOGIN TO YOUR ACCOUNT)
+  "daysTokeep":"14", // Amount of days worth of activity to keep on twitter before deleting it
+  "amountToScan":"50", // Amount of tweets to scan though in every giveaway scan
+  "scanInterval":"5", // Amount of minutes between giveaway scans
+  "amountToScanMessages":"5", // Amount of messages to scan in every directmessage scan
+  "checkMessagesInterval":"10", // Amount of miniutes between directmessage scans
+  "cleanInterval":"300", // Amount of minutes between the automatical deletion function
+  "ghostmode":"True", // Set to Flase to stop the bot performing twitter actions
+  "printMode":"True", // Set to True to allow the bot to print the actions it is performing for debugging
+  "slaveAccounts":[" @BenK360", " @AshtonP_360", " @FaZe__Jimmy", " @josh_herbet"], // Accounts used for the automatic replies to tweets that contain ('tag 3 friends')
+  "tradeLink":"www.steamcommunity.com/tradeoffer/new/?partner=43969537&token=FNUT-F4d" // Steam trade link (this is automatically sent to a user upon request, this allows the user to send the bot owner skins on steam with out the bot owner doing anything)
+}
